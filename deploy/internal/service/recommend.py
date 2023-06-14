9b7f8f82040f93c76fd130e0adee5d3357ace5fd
Gede Gery Sastrawan <gerysastrawan123@gmail.com>
2023-06-14 23:29:33 +0800

    add: condition data

--- deploy/internal/service/recommend.py
+++ deploy/internal/service/recommend.py
@@ -10,10 +10,18 @@ base_dir = "."
 
 
 def recommend_output(gender, category, dataset):
-    if gender == base_dir:
+    path = ""
+    if category == "jacket" or category == "sneakers":
         gender = "all"
+    if gender == "all":
+        path = category
+    else:
+        path = gender+"/"+category
+            
+    # print(gender)
+    # if gender == base_dir:
+    #     gender = "all"
     # path = os.path.join(gender, category)
-    path = gender+"/"+category
     path = os.getenv("IMAGEKIT_ENDPOINT_URL")+"/"+path
     # list of the images in the path
     available_images = dataset[gender][category]
@@ -26,12 +34,13 @@ def recommend_output(gender, category, dataset):
 
 
 class OutfitRecommender:
-    def __init__(self, gender, weather, situation, fashion_style, dataset):
+    def __init__(self, gender, weather, situation, fashion_style, dataset, conditions):
         self.gender = gender
         self.weather = weather
         self.situation = situation
         self.fashion_style = fashion_style
         self.dataset = dataset
+        self.conditions = conditions
 
     def recommend(self):
         gender = self.gender
@@ -39,93 +48,117 @@ class OutfitRecommender:
         situation = self.situation
         fashion_style = self.fashion_style
         dataset = self.dataset
+        conditions = self.conditions
 
-        if gender == "male" and situation == "formal" and fashion_style == "formal":
-            upperwear = recommend_output(laki_dir, 'coat-shirt', dataset)
-            bottomwear = recommend_output(
-                laki_dir, 'trousers', dataset)
-            footwear = recommend_output(laki_dir, 'loafers', dataset)
-            result = {
-                "upperwear": [upperwear],
-                "bottomwear": [bottomwear],
-                "footwear": [footwear],
-            }
-            return result
-        elif gender == "female" and situation == "formal" and fashion_style == "formal":
+        concat = gender+weather+situation+fashion_style
+        key = config.hash_value(concat)
+        if key in conditions.keys():
             upperwear = []
-            val = recommend_output(cewek_dir, 'coat-shirt', dataset)
-            upperwear.append(val)
-            val = recommend_output(cewek_dir, 'dress', dataset)
-            upperwear.append(val)
-
+            for clothe in conditions[key]["upperwear"]:
+                result = recommend_output(gender, clothe, dataset)
+                upperwear.append(result)
             bottomwear = []
-            val = recommend_output(cewek_dir, 'formal-skirt', dataset)
-            bottomwear.append(val)
-            val = recommend_output(cewek_dir, 'trousers', dataset)
-            bottomwear.append(val)
-
+            for clothe in conditions[key]["bottomwear"]:
+                result = recommend_output(gender, clothe, dataset)
+                bottomwear.append(result)
             footwear = []
-            val = recommend_output(cewek_dir, 'loafers', dataset)
-            footwear.append(val)
-            val = recommend_output(cewek_dir, 'heels', dataset)
-            footwear.append(val)
-
-            result = {
-                "upperwear": upperwear,
-                "bottomwear": bottomwear,
-                "footwear": footwear,
+            for clothe in conditions[key]["footwear"]:
+                result = recommend_output(gender, clothe, dataset)
+                footwear.append(result)
+            return {
+                "upperwear":upperwear,
+                "bottomwear":bottomwear,
+                "footwear":footwear,
             }
-            return result
-        elif gender == "male" and situation == "non_formal" and fashion_style != "formal":
-            upperwear = []
-            val = recommend_output(base_dir, 'jacket', dataset)
-            upperwear.append(val)
-            val = recommend_output(laki_dir, 'tshirt', dataset)
-            upperwear.append(val)
-
-            bottomwear = []
-            val = recommend_output(laki_dir, 'trousers', dataset)
-            bottomwear.append(val)
-            val = recommend_output(laki_dir, 'pants', dataset)
-            bottomwear.append(val)
-
-            footwear = []
-            val = recommend_output(base_dir, 'sneakers', dataset)
-            footwear.append(val)
-            val = recommend_output(laki_dir, 'slippers', dataset)
-            footwear.append(val)
-
-            result = {
-                "upperwear": upperwear,
-                "bottomwear": bottomwear,
-                "footwear": footwear,
-            }
-            return result
-        elif gender == "female" and situation == "non_formal" and fashion_style != "formal":
-            upperwear = []
-            val = recommend_output(base_dir, 'jacket', dataset)
-            upperwear.append(val)
-            val = recommend_output(cewek_dir, 'tshirt', dataset)
-            upperwear.append(val)
-            val = recommend_output(cewek_dir, 'onepiece', dataset)
-            upperwear.append(val)
-
-            bottomwear = []
-            val = recommend_output(cewek_dir, 'non-formal-skirt', dataset)
-            bottomwear.append(val)
-            val = recommend_output(cewek_dir, 'trousers', dataset)
-            bottomwear.append(val)
-
-            footwear = []
-            val = recommend_output(base_dir, 'sneakers', dataset)
-            footwear.append(val)
-            val = recommend_output(cewek_dir, 'slippers', dataset)
-            footwear.append(val)
-            result = {
-                "upperwear": upperwear,
-                "bottomwear": bottomwear,
-                "footwear": footwear,
-            }
-            return result
         else:
             return None
+
+        # if gender == "male" and situation == "formal" and fashion_style == "formal":
+        #     upperwear = recommend_output(laki_dir, 'coat-shirt', dataset)
+        #     bottomwear = recommend_output(
+        #         laki_dir, 'trousers', dataset)
+        #     footwear = recommend_output(laki_dir, 'loafers', dataset)
+        #     result = {
+        #         "upperwear": [upperwear],
+        #         "bottomwear": [bottomwear],
+        #         "footwear": [footwear],
+        #     }
+        #     return result
+        # elif gender == "female" and situation == "formal" and fashion_style == "formal":
+        #     upperwear = []
+        #     val = recommend_output(cewek_dir, 'coat-shirt', dataset)
+        #     upperwear.append(val)
+        #     val = recommend_output(cewek_dir, 'dress', dataset)
+        #     upperwear.append(val)
+
+        #     bottomwear = []
+        #     val = recommend_output(cewek_dir, 'formal-skirt', dataset)
+        #     bottomwear.append(val)
+        #     val = recommend_output(cewek_dir, 'trousers', dataset)
+        #     bottomwear.append(val)
+
+        #     footwear = []
+        #     val = recommend_output(cewek_dir, 'loafers', dataset)
+        #     footwear.append(val)
+        #     val = recommend_output(cewek_dir, 'heels', dataset)
+        #     footwear.append(val)
+
+        #     result = {
+        #         "upperwear": upperwear,
+        #         "bottomwear": bottomwear,
+        #         "footwear": footwear,
+        #     }
+        #     return result
+        # elif gender == "male" and situation == "non_formal" and fashion_style != "formal":
+        #     upperwear = []
+        #     val = recommend_output(base_dir, 'jacket', dataset)
+        #     upperwear.append(val)
+        #     val = recommend_output(laki_dir, 'tshirt', dataset)
+        #     upperwear.append(val)
+
+        #     bottomwear = []
+        #     val = recommend_output(laki_dir, 'trousers', dataset)
+        #     bottomwear.append(val)
+        #     val = recommend_output(laki_dir, 'pants', dataset)
+        #     bottomwear.append(val)
+
+        #     footwear = []
+        #     val = recommend_output(base_dir, 'sneakers', dataset)
+        #     footwear.append(val)
+        #     val = recommend_output(laki_dir, 'slippers', dataset)
+        #     footwear.append(val)
+
+        #     result = {
+        #         "upperwear": upperwear,
+        #         "bottomwear": bottomwear,
+        #         "footwear": footwear,
+        #     }
+        #     return result
+        # elif gender == "female" and situation == "non_formal" and fashion_style != "formal":
+        #     upperwear = []
+        #     val = recommend_output(base_dir, 'jacket', dataset)
+        #     upperwear.append(val)
+        #     val = recommend_output(cewek_dir, 'tshirt', dataset)
+        #     upperwear.append(val)
+        #     val = recommend_output(cewek_dir, 'onepiece', dataset)
+        #     upperwear.append(val)
+
+        #     bottomwear = []
+        #     val = recommend_output(cewek_dir, 'non-formal-skirt', dataset)
+        #     bottomwear.append(val)
+        #     val = recommend_output(cewek_dir, 'trousers', dataset)
+        #     bottomwear.append(val)
+
+        #     footwear = []
+        #     val = recommend_output(base_dir, 'sneakers', dataset)
+        #     footwear.append(val)
+        #     val = recommend_output(cewek_dir, 'slippers', dataset)
+        #     footwear.append(val)
+        #     result = {
+        #         "upperwear": upperwear,
+        #         "bottomwear": bottomwear,
+        #         "footwear": footwear,
+        #     }
+        #     return result
+        # else:
+        #     return None
